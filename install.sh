a41971ae035d1738ca456373a4f92064a5a03331
Paul Hammond <paul.hammond@gmail.com>
2022-05-22 18:04:40 +0100

    Pinentry mac setup

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+GPG_AGENT_CONF=gnupg/.gnupg/gpg-agent.conf
+
+mkdir -p gnupg/.gnupg
+
+echo "# See: https://samuelsson.dev/sign-git-commits-on-github-with-gpg-in-macos/" >> $GPG_AGENT_CONF
+echo pinentry-program $(which pinentry-mac) >> $GPG_AGENT_CONF
+
 mv ~/.zshrc ~/.zshrc.old
 
 stow zsh tmux gnupg
