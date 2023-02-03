ff3d0905324680322cbd3225476f0018b5818591
Paul Hammond <paul.hammond@gmail.com>
2023-02-03 10:17:52 +0000

    improve zellij autocompletion (#6)

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ mkdir -p gnupg/.gnupg
 echo "# See: https://samuelsson.dev/sign-git-commits-on-github-with-gpg-in-macos/" >> $GPG_AGENT_CONF
 echo pinentry-program $(which pinentry-mac) >> $GPG_AGENT_CONF
 
-zellij setup --generate-completion zsh >> zsh/.zellij-completion
+# add to fpath and source zellij completion
+FPATH=$FPATH:~/.zsh_autocomplete
+
+mkdir -p ~/.zsh_autocomplete
+zellij setup --generate-completion zsh >> ~/.zsh_autocomplete/.zellij-completion
+
 mv ~/.zshrc ~/.zshrc.old
 
 stow zsh tmux gnupg
