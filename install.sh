9ad8aec2b1259877aea7419b3e831c51c567c5a2
Paul Hammond <paul.hammond@gmail.com>
2023-10-28 08:18:15 +0100

    added zellij config and tweaked alacritty theming
    zellij config
    
    zellij config

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ GPG_AGENT_CONF=gnupg/.gnupg/gpg-agent.conf
 
 mkdir -p gnupg/.gnupg
 
-echo "# See: https://samuelsson.dev/sign-git-commits-on-github-with-gpg-in-macos/" >> $GPG_AGENT_CONF
-echo pinentry-program $(which pinentry-mac) >> $GPG_AGENT_CONF
+echo "# See: https://samuelsson.dev/sign-git-commits-on-github-with-gpg-in-macos/" >>$GPG_AGENT_CONF
+echo pinentry-program $(which pinentry-mac) >>$GPG_AGENT_CONF
 
 # add to fpath and source zellij completion
 FPATH=$FPATH:~/.zsh_autocomplete
 
 mkdir -p ~/.zsh_autocomplete
-zellij setup --generate-completion zsh >> ~/.zsh_autocomplete/_zellij-completion
+zellij setup --generate-completion zsh >>~/.zsh_autocomplete/_zellij-completion
 
 mv ~/.zshrc ~/.zshrc.old
 
-stow zsh tmux gnupg alacritty
+stow zsh tmux gnupg alacritty zellij
