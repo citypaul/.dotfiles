75964d64d5074f43ddc5a32d943f8c49b12fd6d9
Paul Hammond <paul.hammond@gmail.com>
2025-01-12 16:35:34 +0000

    fixed ghostty

--- install.sh
+++ install.sh
@@ -7,16 +7,22 @@ mkdir -p gnupg/.gnupg
 echo "# See: https://samuelsson.dev/sign-git-commits-on-github-with-gpg-in-macos/" >>$GPG_AGENT_CONF
 echo pinentry-program $(which pinentry-mac) >>$GPG_AGENT_CONF
 
-# add to fpath and source zellij completion
+move_with_backup() {
+  local src="$1"
+  local backup="${src}.old"
+
+  if [ -f "$src" ] && [ ! -f "$backup" ]; then
+    mv "$src" "$backup"
+  fi
+}
+
 FPATH=$FPATH:~/.zsh_autocomplete
 
 mkdir -p ~/.zsh_autocomplete
 zellij setup --generate-completion zsh >>~/.zsh_autocomplete/_zellij-completion
 
-mv ~/.zshrc ~/.zshrc.old
+move_with_backup ~/.zshrc
 
-# Remove store old ghostty config, has to be done because opening the app will create a new config file
-# and stow will not allow us to overwrite it.
-mv "$HOME"/ghostty/Library/Application\ Support/com.mitchellh.ghostty/config "$HOME"/ghostty/Library/Application\ Support/com.mitchellh.ghostty/config.old
+move_with_backup "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
 
 stow zsh tmux gnupg alacritty zellij .oh-my-zsh karabiner ghostty
